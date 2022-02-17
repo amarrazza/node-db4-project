eb481af0c622f33ec9115c4c87e4f8adabdb4a46
Austin  Marrazza <austinmarrazza@Austins-MacBook-Pro.local>
2022-02-17 14:28:29 -0800

    set up scaffolding

---
+++ knexfile.js
@@ -0,0 +1,28 @@
+// Update with your config settings.
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+module.exports = {
+
+  development: {
+    client: 'sqlite3',
+    useNullAsDefault: true,
+    connection: {
+      filename: './dev.sqlite3'
+    }
+  },
+  migrations: {
+    directory: './data/migrations',
+  },
+  seeds: {
+    directory: './data/seeds',
+  },
+  // this enables foreign keys in SQLite
+  pool: {
+    afterCreate: (conn, done) => {
+      conn.run('PRAGMA foreign_keys = ON', done)
+    },
+  },
+
+};
